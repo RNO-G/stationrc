d6339ce5c30300f3a7edfb799a4ed3c1e1ec249e
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-11-27 23:46:31 +0100

    fix bug, add option to bring up script

--- bring_up.py
+++ bring_up.py
@@ -13,11 +13,22 @@ parser.add_argument(
     default=3,
     help="Specify version number of config file for CPLDs. Default: 3",
 )
+
+parser.add_argument(
+    "-l",
+    "--enable_remote_logger",
+    action="store_true",
+    help="Enable remote loggert",
+)
+
 args = parser.parse_args()
 
 stationrc.common.setup_logging()
 
 station = stationrc.remote_control.VirtualStation()
+
+if args.enable_remote_logger:
+    station.rc.set_remote_logger_handler()
 
 station.radiant_setup(version=args.version)
 with open(f"peds_{station.get_radiant_board_mcu_uid():032x}.json", "w") as f:
