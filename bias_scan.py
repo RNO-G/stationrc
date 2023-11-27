cc70601a1f28d8c0baa234dde6a1c32a35fb075b
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-11-27 23:12:01 +0100

    Refactoring of bias_scan.py

--- bias_scan.py
+++ bias_scan.py
@@ -1,133 +1,128 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
-import time
-import scipy
+from scipy import optimize
+import argparse
 
-import stationrc.radiant
 import stationrc.remote_control
-import pandas as pd
+import stationrc.common
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-c",
+    "--channel",
+    type=int,
+    default=range(24),
+    nargs="*",
+    help="Plot certain channel. If None plot all channels. (Default: None)",
+)
+
+parser.add_argument("--start", type=int, default=0, nargs="?")
+parser.add_argument("--stop", type=int, default=2000, nargs="?")
+parser.add_argument("--points", type=int, default=10, nargs="?")
+
+args = parser.parse_args()
+
+stationrc.common.setup_logging()
 
 station = stationrc.remote_control.VirtualStation()
 
-def bias_scan(start, end, points): 
-    
-    channels = range(0, 24)
-    ped = [[]]*len(channels)
-    tot_ped = [[]]*len(channels)
-    scan = []
-    interval = np.arange(int(start), int(end), int((end-start)/points))
-    
-    for v in interval:
+station = stationrc.remote_control.VirtualStation()
+
+def bias_scan(start, end, points):
+
+    dac_input = np.arange(int(start), int(end), int((end - start) / points))
+
+    pedestals = np.zeros((len(dac_input), 24, 4096))
+
+    for idx, v in enumerate(dac_input):
         station.radiant_pedestal_set(value = int(v))
-        pedestal_at_v = station.radiant_pedestal_get()
+        pedestal_at_v = np.array(station.radiant_pedestal_get())
 
-        for ch in channels: 
-        	# averaged pedestal
-            ped[ch] = ped[ch] + [np.mean(pedestal_at_v[ch])]
-            #gets the pedestal of all 4096 storage cells
-            tot_ped[ch] = tot_ped[ch] + [pedestal_at_v[ch]]
+        pedestals[idx] = pedestal_at_v
 
-    bias_scan = np.array(interval)
-    pedestal = [np.array(j) for j in ped]
-    
-    return bias_scan, pedestal, tot_ped
-    
-def line(x, m, b): 
+    return dac_input, pedestals
+
+def line(x, m, b):
     return m * x + b
 
 def fit(adc, samples):
-    opt, _ = scipy.optimize.curve_fit(line, xdata=adc, ydata=samples)
-    lin_fit = [opt[0] * x + opt[1] for x in adc]
+    opt, _ = optimize.curve_fit(line, xdata=adc, ydata=samples)
+    lin_fit = opt[0] * adc + opt[1]
     return lin_fit, opt[0], opt[1]
 
-def check_coeff(m, b):
-    m_min = 1.0
-    m_max = 1.3
-    b_min = 150.0
-    b_max = 250.0
-    if ((m_min < m < m_max) & (b_min < b < b_max)):
-        print('PASSED')
-    else:
-        print('FAILED')
 
-#input ADC counts
-start = 0 
-end = 2000 
-points = 10
+dac_input, pedestals = bias_scan(args.start, args.stop, args.points)
+mean_pedestal_per_channel = np.mean(pedestals, axis=-1)
 
-adc_list, samples, tot_pedestal = bias_scan(start, end, points)
+print(mean_pedestal_per_channel.shape)
+plt.plot(pedestals[0][0])
+plt.xlabel("samples")
+plt.ylabel("ADC")
+plt.show(block=False)
 
-#Change channel here
-channel = 0
+nrows, ncols = 6, 4
+fig, axs = plt.subplots(nrows, ncols, figsize=(10, 10), sharex=True, sharey=True)
 
-#prepare the data before the fit
-horizontal_arr = [[]]*len(tot_pedestal[channel][0]) #shape 4096*10
-for i in range(len(tot_pedestal[channel][0])):
-    for i_v in range(len(adc_list)):
-        horizontal_arr[i] = horizontal_arr[i] + [tot_pedestal[channel][i_v][i]]
-#fit
-fit_ped = [[]]*len(horizontal_arr)
-channel = 0
-for ind in range(len(horizontal_arr)):
-    ft = fit(adc_list, horizontal_arr[ind])[0]
-    #print(fit(adc_list, horizontal_arr[ind])[1], fit(adc_list, horizontal_arr[ind])[2])
-    fit_ped[ind] = fit_ped[ind]+ft
+for ch, mean_pedestal in enumerate(mean_pedestal_per_channel.T):
+    print(dac_input, mean_pedestal)
+    lin_fit, a, b = fit(dac_input, mean_pedestal)
+    print(lin_fit, a, b)
+    axs.flatten()[ch].plot(dac_input, mean_pedestal, '.', label=f'data, CH {ch}')
+    axs.flatten()[ch].plot(dac_input, lin_fit, '--')
+    axs.flatten()[ch].legend(fontsize=5)
 
-df0 = pd.DataFrame(np.array(fit_ped), columns=adc_list)
-#to save and read
-#df0.to_csv("tot_pedestal_fit_CH0.csv")
-#df0 = pd.read_csv("tot_pedestal_fit_CH0.csv", index_col=0)
 
-plt.rcParams["figure.figsize"] = (7, 5)
-for i in range(len(df0)):
-    plt.plot(list(df0.columns), horizontal_arr[i], '.', label='data, CH'+str(ch))
-    plt.plot(list(df0.columns), df0.iloc[i], '--', label='fit', linewidth=0.5)
-plt.xlabel('input ADC')
-plt.ylabel('V bias')
-plt.grid()
-#plt.legend()
-plt.show()
+fig.supxlabel('DAC input')
+fig.supylabel('ADC output')
+fig.tight_layout()
+plt.show(block=False)
 
-#fit averaged pedestals and plot
-fit_array = [[]]*24
+# for channel in channels:
 
-for ch in range(0,24):
-    ft = fit(adc_list, samples[ch])
-    fit_array[ch] = fit_array[ch]+ft
-    
-df_fit = pd.DataFrame(np.array(fit_array), columns=adc_list)
+#     #prepare the data before the fit
+#     horizontal_arr = [[]]*len(tot_pedestal[channel][0]) #shape 4096*10
+#     for i in range(len(tot_pedestal[channel][0])):
+#         for i_v in range(len(adc_list)):
+#             horizontal_arr[i] = horizontal_arr[i] + [tot_pedestal[channel][i_v][i]]
 
-#to save and read
-#df_fit.to_csv("bias_scan_fit.csv")
-#df_fit = pd.read_csv("bias_scan_fit.csv", index_col=0)
+#     #fit
+#     fit_ped = [[]]*len(horizontal_arr)
+#     for ind in range(len(horizontal_arr)):
+#         ft = fit(adc_list, horizontal_arr[ind])[0]
+#         fit_ped[ind] = fit_ped[ind]+ft
 
-plt.rcParams["figure.figsize"] = (3,0.5)
-for ch in range(0, 24):
-    plt.plot(list(df_fit.columns), samples[ch], '.', label='data, CH'+str(ch))
-    plt.plot(list(df_fit.columns), df.iloc[ch], '--', label='fit', linewidth=0.5)
-    plt.xlabel('ADC')
-    plt.ylabel('V bias')
-    plt.grid()
-	plt.legend()
-    plt.show()
+#     df0 = pd.DataFrame(np.array(fit_ped), columns=adc_list)
+
+#     ax = axs[channel // ncols][channel % ncols]
+#     for i in range(len(df0)):
+#         ax.plot(list(df0.columns), horizontal_arr[i], '.') #data for channel
+#         ax.plot(list(df0.columns), df0.iloc[i], '--', linewidth=0.5) #fit
+#     ax.set_xlabel('input ADC')
+#     ax.set_ylabel('V bias')
+# fig.tight_layout()
+# #plt.savefig("bias_scan_total_fit.png")
+# #plt.close()
+# plt.show(block=False)
+
 
 #plot all pedestal samples with gradient
-plt.rcParams["figure.figsize"] = (10,3)
-#change channel here
-channel = 0
+fig, axs = plt.subplots(4, 6, figsize=(16, 8), sharex=True, sharey=True, layout='constrained')
 
-bias = range(adc_list[0], adc_list[-1], int((adc_list[-1]-adc_list[0])/10)) #y
-num_samples = range(len(tot_pedestal[channel][0])) #x
-# z = relation between x and y: for every value of bias the sample has a corresponding pedestal value
-#z dim has to be x * y 
-z = tot_pedestal[channel]
+bias = range(dac_input[0], dac_input[-1], int((dac_input[-1] - dac_input[0]) / len(dac_input))) #y
 
-c = plt.imshow(z, cmap ='plasma', 
-               extent =[min(num_samples), max(num_samples), min(bias), max(bias)],
-               interpolation ='nearest', origin ='lower')
-plt.colorbar(c)
-plt.xlabel('Sample')
-plt.ylabel('Bias Input (ADC)')
-plt.title('bias scan, CH'+str(channel), fontweight ="ultralight")
+# v_adc, channel, sample -> channel, v_adc, sample
+pedestals = np.swapaxes(pedestals, 0, 1)
+
+for idx, (ax, peds) in enumerate(zip(axs.flatten(), pedestals)):
+    c = ax.imshow(peds, cmap ='plasma',
+                   extent =[0, 4095, min(bias), max(bias)],
+                   interpolation ='nearest', origin ='lower')
+
+cbr = fig.colorbar(c, ax=axs.ravel().tolist(), pad=0.02)
+cbr.set_label(r"ADC output")
+fig.supxlabel('samples')
+fig.supylabel('DAC input')
+# fig.subplots_adjust(hspace=0.02)
 plt.show()
