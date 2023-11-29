8394bbbe02e2feac27b2939b4a04e38fb9b2e9f8
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-11-29 10:33:38 +0100

    Re-add flipping of seam sample ...

--- stationrc/remote_control/util.py
+++ stationrc/remote_control/util.py
@@ -208,9 +208,9 @@ def update_seam_and_slow(station, channel, frequency, tune_mode, nom_sample):
     if np.sum(t[channel][1:128]) > nom_sample * 127.68:
         logging.warning(
             f"Feedback LAB{channel} way off ({nom_sample * 127 - np.sum(t[channel][1:128]):.2f}): "
-            f"{t[channel][0]} -> {-1 * t[channel][0]:.2f}"
+            f"Flip seam sample {t[channel][0]:.2f} -> {-1 * t[channel][0]:.2f}"
         )
-        t[channel][0] *= -1
+        seamSample *= -1
 
     return t, seamSample, slowSample
 
@@ -385,9 +385,7 @@ def initial_tune(station, channel, frequency=510, max_tries=50, bad_lab=False, e
 
         curTry += 1
 
-    t = get_time_run(station, frequency * 1e6)
-    seamSample = t[channel][0]
-    slowSample = t[channel][127]
+    t, seamSample, slowSample = update_seam_and_slow(station, channel, frequency, "seam", nom_sample)
 
     # dumb way to check if we're boucing around the nominal range set by slow and fast factors.
     # If it bounces then adjust slow, which is likely off, then continue with seam (mean)
