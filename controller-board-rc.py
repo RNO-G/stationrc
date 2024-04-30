a7c554b55c6184eddbaba8ca34bfc5e16ef2b37d
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-04-30 09:25:07 -0500

    Allow to switch on and off several stations in a row

--- controller-board-rc.py
+++ controller-board-rc.py
@@ -8,17 +8,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument("command", type=str, help="command to be sent to station")
 
 parser.add_argument(
-    "--host",
-    type=str,
-    default=None,
-    help="Specify ip address of host. If `None`, use ip from `virtual_station_config.json`."
-)
+    "--host", "--hosts",
+    dest="hosts",
+    type=str, default=[None],
+    nargs="+",
+    help="Specify ip address of host. If `None`, use ip from config in stationrc.")
+
 
 args = parser.parse_args()
 
 stationrc.common.setup_logging()
 
-station = stationrc.remote_control.VirtualStation(host=args.host)
+for host in args.hosts:
+    station = stationrc.remote_control.VirtualStation(host=host)
 
-data = station.rc.send_command("controller-board", args.command)
-print(data)
+    data = station.rc.send_command("controller-board", args.command)
+    print(data)
