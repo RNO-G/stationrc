25fe134dc2c79b921e2368730edd6c46e6073f9f
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-07-06 16:27:44 -0100

    Extend plotting in record_data.py

--- record_data.py
+++ record_data.py
@@ -88,6 +88,12 @@ parser.add_argument(
     help="Store plots"
 )
 
+parser.add_argument(
+    "--all",
+    action="store_true",
+    help="Plot all channels in an individual axis"
+)
+
 args = parser.parse_args()
 
 stationrc.common.setup_logging()
@@ -103,13 +109,34 @@ if args.save or args.save_data:
     uid = station.get_radiant_board_mcu_uid()
 
 for idx, ev in enumerate(data["data"]["WAVEFORM"]):
-    fig, ax = plt.subplots()
-    if args.channel is None:
-        for ch, wvf in enumerate(ev["radiant_waveforms"]):
+    if args.all:
+        fig, axs = plt.subplots(4, 6, figsize=(12, 8), gridspec_kw=dict(hspace=0.05, wspace=0.05), sharex=True, sharey=True)
+        for ch, (wvf, ax) in enumerate(zip(ev["radiant_waveforms"], axs.flatten())):
             ax.plot(wvf, marker=args.marker, label=f"ch. {ch}")
+            ax.legend()
+            if args.line:
+                for i in range(16):
+                    ax.axvline(i * 128, color="k", lw=1, zorder=0)
+        fig.supxlabel("samples")
+        fig.supylabel("ADC counts")
     else:
-        for channel in args.channel:
-            ax.plot(ev["radiant_waveforms"][channel], marker=args.marker, label=f"ch. {channel}", lw=1)
+        fig, ax = plt.subplots()
+
+        if args.channel is None:
+            for ch, wvf in enumerate(ev["radiant_waveforms"]):
+                ax.plot(wvf, marker=args.marker, label=f"ch. {ch}")
+        else:
+            for channel in args.channel:
+                ax.plot(ev["radiant_waveforms"][channel], marker=args.marker, label=f"ch. {channel}", lw=1)
+
+        if args.line:
+            for i in range(16):
+                ax.axvline(i * 128, color="k", lw=1, zorder=0)
+
+        ax.legend()
+        ax.set_title(f"Event: {ev['event_number']}")
+        ax.set_xlabel("samples")
+        ax.set_ylabel("ADC counts")
 
     if args.range is not None:
         if len(args.range) == 1:
@@ -120,14 +147,7 @@ for idx, ev in enumerate(data["data"]["WAVEFORM"]):
         else:
             raise ValueError("Wrong length for args.range (only 1 or 2 are allowed)")
 
-    if args.line:
-        for i in range(16):
-            ax.axvline(i * 128, color="k", lw=1, zorder=0)
 
-    ax.legend()
-    ax.set_title(f"Event: {ev['event_number']}")
-    ax.set_xlabel("Sample")
-    ax.set_ylabel("ADC counts")
     if args.save:
         fig.tight_layout()
         plt.savefig(f"waveform_ch{args.channel}_{idx}_{uid:032x}", transparent=False)
