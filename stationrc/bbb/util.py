2d8ce11988b6f0ffac5530449ad4cd5623f6fe28
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2024-10-14 18:43:50 -0500

    harmonized way of talking to controller board

--- stationrc/bbb/util.py
+++ stationrc/bbb/util.py
@@ -1,10 +1,4 @@
-from stationrc.bbb.ControllerBoard import run_command_controller_board
 import sys, os, time
 
-def power_cycle_radiant():
-    if not os.path.exists("/dev/ttyController"):
-        sys.exit("Need to call this function on the BBB!")
-
-    run_command_controller_board("#RADIANT-OFF", read_response = False)
-    time.sleep(2)
-    run_command_controller_board("#RADIANT-ON", read_response = False)
+def on_bbb():
+    return os.path.exists("/dev/ttyRadiant")
