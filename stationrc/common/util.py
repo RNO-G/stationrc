38532c018684a6161fc15d6b05be9d122b2ca1df
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-04-24 15:02:58 +0200

    Add some code to common/util.py for easy access to binary data

--- stationrc/common/util.py
+++ stationrc/common/util.py
@@ -3,8 +3,8 @@ import logging
 import logging.config
 import pathlib
 
-from .Executor import Executor
-from .RNOGDataFile import RNOGDataFile
+from stationrc.common.Executor import Executor
+from stationrc.common.RNOGDataFile import RNOGDataFile
 
 def rootify(data_dir, mattak_dir="", logger=logging.getLogger("root")):
     datadir = pathlib.Path(data_dir)
@@ -114,3 +114,51 @@ def setup_logging():
     with open(pathlib.Path(__file__).parent / "conf" / "logging_conf.json", "r") as f:
         conf = json.load(f)
     logging.config.dictConfig(conf)
+
+
+if __name__ == "__main__":
+    import sys
+    import os
+
+    if not os.path.isdir(sys.argv[1]):
+        raise ValueError(f"The (run) directory {sys.argv[1]} does not exist!")
+
+    waveforms_dir = os.path.join(sys.argv[1], "waveforms")
+    if not os.path.exists(waveforms_dir):
+        raise ValueError(f"Waveforms directory {waveforms_dir} does not exist!")
+
+    waveform_files = os.listdir(waveforms_dir)
+    if len(waveform_files) == 0:
+        raise ValueError(f"Waveforms directory {waveforms_dir} is empty!")
+
+    print(f"Found {len(waveform_files)} waveform files in {waveforms_dir}.")
+    waveform_files = sorted(waveform_files)
+
+    headers_dir = os.path.join(sys.argv[1], "header")
+
+    for idx, wf_file in enumerate(waveform_files):
+        wf_file_path = os.path.join(waveforms_dir, wf_file)
+        hdr_file_path = os.path.join(headers_dir, wf_file.replace("wf", "hd"))
+
+        data = dump_binary(
+            wf_file_path,
+            read_header=os.path.exists(hdr_file_path),
+            hdr_file=hdr_file_path
+        )
+
+        if "HEADER" in data:
+            hdr_data = data["HEADER"]
+
+        for event_idx, event_wf_data in enumerate(data["WAVEFORM"]):
+            event_number = event_wf_data["event_number"]
+            run_number = event_wf_data["run_number"]
+            station = event_wf_data["station"]
+            radiant_sampling_rate = event_wf_data["radiant_sampling_rate"]
+            wfs = event_wf_data["radiant_waveforms"]
+
+            if "HEADER" in data:
+                header = hdr_data[event_idx]
+                assert header["event_number"] == event_number, "Missmatch between wf and hdr data"
+                trigger_type = header["trigger_type"]
+
+            # do something with the data
